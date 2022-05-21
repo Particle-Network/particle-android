0d9054d6e7fabf307ddb1fa79b920dcea093e928
Houn <xuxihong@minijoy.work>
2022-05-21 15:19:41 +0800

    add wallet service init method

--- app/src/main/java/com/particle/demo/App.kt
+++ app/src/main/java/com/particle/demo/App.kt
@@ -1,6 +1,8 @@
 package com.particle.demo
 
 import android.app.Application
+import com.particle.gui.ParticleNetworkGUI
+import com.particle.gui.utils.CrashHandler
 import com.particle.network.ChainId
 import com.particle.network.ChainName
 import com.particle.network.Env
@@ -9,6 +11,10 @@ import com.particle.network.ParticleNetwork
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        ParticleNetwork.init(this, Env.DEV, ChainName.Solana, ChainId.SolanaDevnet)
+        CrashHandler().init(this)
+//        auth service
+//        ParticleNetwork.init(this, Env.DEV, ChainName.Solana, ChainId.SolanaDevnet)
+        // auth + wallet service
+        ParticleNetworkGUI.init(this, Env.DEV, ChainName.Solana, ChainId.SolanaDevnet)
     }
 }
